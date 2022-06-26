00eb1df130732a74b2a7526860cb54894e00c39e
Gabrielle Rosane Santos <71800921@aluno.faculdadecotemig.br>
2022-06-26 12:30:04 -0300

    add add base structure

---
+++ src/main/java/com/grs/PublisherConsumerMqVehiclesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.grs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublisherConsumerMqVehiclesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublisherConsumerMqVehiclesServiceApplication.class, args);
+	}
+
+}
